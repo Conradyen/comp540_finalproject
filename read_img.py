81324bdeae4b2d7803cd31791d1530a93d3f1332
Conradyen <36934379+Conradyen@users.noreply.github.com>
2018-03-11 22:40:02 -0500

    Add files via upload

---
+++ read_img.py
@@ -0,0 +1,60 @@
+import numpy as np
+from tqdm import tqdm
+from skimage.io import imread, imshow, imread_collection, concatenate_images
+import os
+import sys
+from skimage.transform import resize
+import numpy as np
+
+
+TRAIN_PATH = '/Users/yen/Documents/COMP 540/final_project/data_set/stage1_train/'
+TEST_PATH = '/Users/yen/Documents/COMP 540/final_project/data_set/stage1_test/'
+
+train_ids = next(os.walk(TRAIN_PATH))[1]
+test_ids = next(os.walk(TEST_PATH))[1]
+
+def read_train(IMG_WIDTH = 256,IMG_HEIGHT = 256,IMG_CHANNELS = 3):
+    '''
+    convert image into 3D numpy array
+    '''
+    IMG_WIDTH = 256
+    IMG_HEIGHT = 256
+    IMG_CHANNELS = 3
+
+    X_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+    Y_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+    print('Getting train images and masks ... ')
+    sys.stdout.flush()
+
+    for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
+        path = TRAIN_PATH + id_
+        img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+        img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+        X_train[n] = img
+        mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+        for mask_file in next(os.walk(path + '/masks/'))[2]:
+            mask_ = imread(path + '/masks/' + mask_file)
+            mask_ = np.expand_dims(resize(mask_, (IMG_HEIGHT, IMG_WIDTH), mode='constant',
+                                          preserve_range=True), axis=-1)
+            mask = np.maximum(mask, mask_)
+        Y_train[n] = mask
+
+    return X_train,Y_train
+
+def resd_test(IMG_WIDTH = 256,IMG_HEIGHT = 256,IMG_CHANNELS = 3):
+    '''
+    read test image
+    '''
+
+    X_test = np.zeros((len(test_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+    sizes_test = []
+    print('Getting test images ... ')
+    sys.stdout.flush()
+
+    for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
+        path = TEST_PATH + id_
+        img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+        sizes_test.append([img.shape[0], img.shape[1]])
+        img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+        X_test[n] = img
+    return X_test
